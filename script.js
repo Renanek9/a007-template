19bad58ec434b5b8ec374412281961fe4dd5cb3c
Renan Lopes <r.maruffa@gmail.com>
2022-07-18 22:07:10 -0300

    Exercicios da aula 007

---
+++ script.js
@@ -0,0 +1,59 @@
+// let nome = prompt("Qual seu nome?")
+// console.log(nome)
+// let idade = prompt("Qual a sua idade?") 
+// console.log(idade);
+
+// nome = Number("Roberson")
+// idade = Number("35")
+// let redeSocial = "tinder"
+// console.log(nome);
+// console.log(idade);
+// console.log(redeSocial);
+
+// console.log("O nome dele é", nome, "e ele tem", idade, "e eu conheci ele no", redeSocial);
+
+// let tenhoPet = false
+// const nomeDoPet = undefined
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+// console.log(typeof tenhoPet);
+// console.log(typeof nomeDoPet);
+
+// Exercicio de Fixação 1
+// Pense em um objeto, e a partir deste objeto, crie 3 variáveis. Atribua valores às 3 variáveis: 
+// const String;
+// let Number;
+// let Boolean.
+// Utilize typeof para checar se os tipos estão corretos;
+// Imprima as três variáveis numa frase, utilizando console.log
+
+// const carro = "Citroen"
+// let anoDeProducao = 1998
+// let eleAnda = false
+// console.log(carro);
+// console.log(anoDeProducao);
+
+// console.log(typeof carro);
+// console.log(typeof anoDeProducao);
+// console.log(typeof eleAnda);
+
+// console.log("Eu tenho um", carro, "e ele é ano", anoDeProducao);
+
+
+// Exercicio de Fixação 2
+// Crie duas variáveis, A e B, e atribua um valor numérico para cada uma delas;
+// Crie uma terceira variável, C, e não atribua valor a ela;
+// Agora, reatribua os valores, de forma a trocá-los de lugar. Assim, o valor de A seria o valor de B, e vice-versa.
+// Exemplo: A=30, B=20. Ao fim do nosso código, os valores devem ser: A=20, B=30
+
+// let a = 30
+// let b = 20
+// let c
+
+// c=a
+// a=b
+// b=c
+
+// console.log("A",a)
+// console.log("B",b)
